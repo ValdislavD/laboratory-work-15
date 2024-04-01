76dd9f75eb599c3a6a08fd97a9cf3e6d91a104a5
VladislavD <vladislavdmuhov792@gmail.com>
2024-04-02 00:01:58 +0300

    The third task

--- tasks.c
+++ tasks.c
@@ -66,6 +66,69 @@ void sortRowsByMaxElement(matrix *m) {
     *m = sorted;
 }
 
+// Функция для нахождения минимального элемента в массиве
+int getMin(int *a, int n) {
+    int min = a[0];
+    for (int i = 1; i < n; i++) {
+        if (a[i] < min) {
+            min = a[i];
+        }
+    }
+    return min;
+}
+
+// Функция для упорядочивания столбцов матрицы по неубыванию минимальных элементов столбцов
+void sortColsByMinElement(matrix *m) {
+    int nCols = m->nCols;
+    // Создаем временный массив для хранения минимальных элементов столбцов и их индексов
+    int *minValues = (int *)malloc(nCols * sizeof(int));
+    int *indices = (int *)malloc(nCols * sizeof(int));
+
+    // Заполняем массивы минимальными значениями и индексами столбцов
+    for (int j = 0; j < nCols; j++) {
+        int *column = (int *)malloc(m->nRows * sizeof(int));
+        for (int i = 0; i < m->nRows; i++) {
+            column[i] = m->values[i][j];
+        }
+        minValues[j] = getMin(column, m->nRows);
+        indices[j] = j;
+        free(column);
+    }
+
+    // Сортируем массив индексов столбцов по соответствующим минимальным значениям
+    for (int i = 1; i < nCols; i++) {
+        int key = minValues[i];
+        int index = indices[i];
+        int j = i - 1;
+
+        while (j >= 0 && minValues[j] > key) {
+            minValues[j + 1] = minValues[j];
+            indices[j + 1] = indices[j];
+            j = j - 1;
+        }
+        minValues[j + 1] = key;
+        indices[j + 1] = index;
+    }
+
+    // Создаем временную матрицу для хранения отсортированных столбцов
+    matrix sorted = getMemMatrix(m->nRows, m->nCols);
+
+    // Копируем столбцы в порядке, определенном отсортированным массивом индексов
+    for (int j = 0; j < nCols; j++) {
+        for (int i = 0; i < m->nRows; i++) {
+            sorted.values[i][j] = m->values[i][indices[j]];
+        }
+    }
+
+    // Освобождаем память от временных массивов
+    free(minValues);
+    free(indices);
+
+    // Заменяем исходную матрицу на отсортированную
+    freeMemMatrix(m);
+    *m = sorted;
+}
+
 int main() {
     SetConsoleOutputCP(CP_UTF8);
     // Создаем и заполняем квадратную матрицу
@@ -87,6 +150,21 @@ int main() {
     // Освобождаем память от матриц
     freeMemMatrix(&squareMatrix);
     freeMemMatrix(&matrixForSorting);
+
+    // Создаем и заполняем матрицу
+    matrix myMatrix = createMatrixFromArray((const int[]){
+            3, 5, 2, 4, 3, 3,
+            2, 5, 1, 8, 2, 7,
+            6, 1, 4, 4, 8, 3
+    }, 3, 6);
+    outputMatrix(myMatrix);
+
+    // Задача 3: Упорядочить столбцы матрицы по неубыванию минимальных элементов столбцов
+    sortColsByMinElement(&myMatrix);
+    outputMatrix(myMatrix);
+
+    // Освобождаем память от матрицы
+    freeMemMatrix(&myMatrix);
 
     return 0;
 }
